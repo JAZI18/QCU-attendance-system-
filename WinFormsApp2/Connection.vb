f89559890e004210ee23401962f49c8b139ce4b4
Enerjhun relon <enerjhunrelon@gmail.com>
2023-11-22 16:35:55 +0800

    added connection module and references

---
+++ WinFormsApp2/Connection.vb
@@ -0,0 +1,32 @@
+﻿
+
+
+Imports MySql.Data.MySqlClient
+
+Module Connection
+
+
+    Private conString = ""
+    Public con As New MySqlConnection(conString)
+
+
+    Sub openCon()
+        con.Open()
+    End Sub
+
+
+    'mysqlcommand creation helper
+    Function newCommand(query As String) As MySqlCommand
+        Return New MySqlCommand(query, con)
+    End Function
+
+    'opens connection and creates mysqlcommand 
+    Function newQuery(query As String) As MySqlCommand
+        openCon()
+        Return newCommand(query)
+    End Function
+
+
+
+
+End Module
