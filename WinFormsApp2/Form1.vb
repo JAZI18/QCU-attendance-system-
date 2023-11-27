8f4b252d433a0d1a83098296d861d12f987e5a5f
Enerjhun relon <enerjhunrelon@gmail.com>
2023-11-27 13:25:19 +0800

    added test for queries

--- WinFormsApp2/Form1.vb
+++ WinFormsApp2/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -13,6 +15,7 @@
 
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
 
+
     End Sub
 
     Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
@@ -20,10 +23,30 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        If TextBox1.Text() = "admin" And TextBox2.Text() = "admin" Then
-            Me.Hide()
-            form2.Show()
 
+
+
+        insertQuery("administrator", "password", {"123"})
+        updateQuery("administrator", "password", {"0"})
+        deleteQuery("administrator", "1", {})
+        Dim reader As MySqlDataReader = selectQuery("Password", "administrator")
+
+        If reader.Read() Then
+            MsgBox(reader("Password"))
         End If
+
+        deleteQuery("administrator", "1", {})
+        Dim reader2 As MySqlDataReader = selectQuery("Password", "administrator")
+
+        If reader2.Read() Then
+            MsgBox(reader2("Password"))
+        End If
+
+
+        'If TextBox1.Text() = "admin" And TextBox2.Text() = "admin" Then
+        '    Me.Hide()
+        '    form2.Show()
+
+        'End If
     End Sub
 End Class
