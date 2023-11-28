c17e9b9f42e3257ee3d699df29732e85d490c01e
mariano.jordan.carvero@gmail.com <mariano.jordan.carvero@gmail.com>
2023-11-28 16:05:36 +0800

    login clean

--- WinFormsApp2/loginform.vb
+++ WinFormsApp2/loginform.vb
@@ -1,30 +1,11 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class loginform
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
-
-
-    End Sub
-
-    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
-
-    End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles login_btn.Click
 
-        Dim reader As MySqlDataReader = selectQuery("password", "admin_account", {un_tb.Text}, "username = @username")
+        Dim reader As MySqlDataReader = SelectQuery("password", "admin_account", {un_tb.Text}, "username = @username")
 
         If Not reader.Read() Then
             invalid_lb.Visible = True
