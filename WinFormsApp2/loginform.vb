5fed01c54541f4f26b292a9f0741273ce1b71679
alex tares <alexander.estares77@gmail.com>
2023-11-28 17:02:11 +0800

    Revert "merge"
    This reverts commit 113845ba403817ae9e8d3c84357001234763c095.

--- WinFormsApp2/loginform.vb
+++ WinFormsApp2/loginform.vb
@@ -1,26 +1,7 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class loginform
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
-
-
-    End Sub
-
-    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
-
-    End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles login_btn.Click
 
@@ -46,6 +27,13 @@ Public Class loginform
     End Sub
 
     Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub mainform_back_btn_Click(sender As Object, e As EventArgs) Handles mainform_back_btn.Click
+
+        staffadminbtns.Show()
+        Me.Close()
 
     End Sub
 End Class
