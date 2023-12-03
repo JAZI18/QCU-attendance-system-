93d9ef8bb97697fa95bb82edc3a5c7a605a8f7ec
Enerjhun relon <enerjhunrelon@gmail.com>
2023-12-03 19:31:06 +0800

    nav update

--- WinFormsApp2/nav.vb
+++ WinFormsApp2/nav.vb
@@ -2,35 +2,56 @@
     Inherits Panel
 
     Private _selected As Button
-    Public highlightBackColor As Color
-    Public normalBackColor As Color
-    Public buttons As Button()
 
+    Private _highlightBackColor As Color
+    Private _normalBackColor As Color
+    Public buttons As New Collection
+
+    Public Property HighlightBackColor As Color
+        Get
+            Return _highlightBackColor
+        End Get
+        Set(value As Color)
+            _highlightBackColor = value
+        End Set
+    End Property
+
+    Public Property NormalBackColor As Color
+        Get
+            Return _normalBackColor
+        End Get
+        Set(value As Color)
+            _normalBackColor = value
+        End Set
+    End Property
 
     Protected Overrides Sub OnCreateControl()
         MyBase.OnCreateControl()
+        AddHandler FindForm().Load, Sub()
+                                        For Each btn As Button In buttons
+                                            AddBtnHandler(btn)
+                                        Next
+                                    End Sub
+    End Sub
 
-        For Each btn As Button In buttons
-
-
-
-            AddHandler btn.Click, Sub()
-                                      If _selected Is _selected Then Exit Sub
-                                      HandleSelect(btn)
-                                  End Sub
-        Next
+    Private Sub AddBtnHandler(btn As Button)
+        AddHandler btn.Click, Sub()
+                                  If _selected Is btn Then Exit Sub
+                                  HandleSelect(btn)
+                              End Sub
     End Sub
 
     Private Sub HandleSelect(btn)
-        _selected.BackColor = normalBackColor
+        If _selected IsNot Nothing Then _selected.BackColor = NormalBackColor
         _selected = btn
-        _selected.BackColor = highlightBackColor
+        _selected.BackColor = HighlightBackColor
     End Sub
 
     Protected Overrides Sub OnControlAdded(e As ControlEventArgs)
         If (TypeOf e.Control Is Button) Then
+            Dim btn = e.Control
+            buttons.Add(btn)
             MyBase.OnControlAdded(e)
-            buttons.Append(e.Control)
             Return
         End If
     End Sub
