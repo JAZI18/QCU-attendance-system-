1a0109f7217354ca8fb86f55f63481c2c8820ed5
Enerjhun relon <enerjhunrelon@gmail.com>
2023-12-03 19:30:52 +0800

    custom nav group added

---
+++ WinFormsApp2/nav.vb
@@ -0,0 +1,37 @@
+﻿Public Class nav
+    Inherits Panel
+
+    Private _selected As Button
+    Public highlightBackColor As Color
+    Public normalBackColor As Color
+    Public buttons As Button()
+
+
+    Protected Overrides Sub OnCreateControl()
+        MyBase.OnCreateControl()
+
+        For Each btn As Button In buttons
+
+
+
+            AddHandler btn.Click, Sub()
+                                      If _selected Is _selected Then Exit Sub
+                                      HandleSelect(btn)
+                                  End Sub
+        Next
+    End Sub
+
+    Private Sub HandleSelect(btn)
+        _selected.BackColor = normalBackColor
+        _selected = btn
+        _selected.BackColor = highlightBackColor
+    End Sub
+
+    Protected Overrides Sub OnControlAdded(e As ControlEventArgs)
+        If (TypeOf e.Control Is Button) Then
+            MyBase.OnControlAdded(e)
+            buttons.Append(e.Control)
+            Return
+        End If
+    End Sub
+End Class
