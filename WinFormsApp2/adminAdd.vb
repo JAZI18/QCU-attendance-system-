1cedcbea78ccdc083c56f154762246210ded360b
alex tares <alexander.estares77@gmail.com>
2023-12-05 03:55:04 +0800

    debug Addmin Add

--- WinFormsApp2/adminAdd.vb
+++ WinFormsApp2/adminAdd.vb
@@ -1,6 +1,7 @@
 ﻿
 Imports System.Security.Cryptography
 Imports System.Text
+Imports MySql.Data.MySqlClient
 
 Public Class adminAdd
 
@@ -18,18 +19,33 @@ Public Class adminAdd
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim hashedPasswords As String = HashedPassword(TextBox1.Text)
+        Dim reader As MySqlDataReader = SelectQuery("*", "admin_account", {_employeeCode}, "employee_info = @params")
+
+        If TextBox1.Text = "" Or e_firstname.Text = "" Then
+
+            MsgBox("Please fill all empty field")
+        Else
+            If reader.Read Then
+                MsgBox("It has already an admin account")
+                Me.Close()
+            Else
+
+                Try
+                    InsertQuery("admin_account", "username,password,employee_info", {e_firstname.Text, hashedPasswords, _employeeCode})
+                    MessageBox.Show("Record inserted successfully.")
+                    Me.Close()
+
+                Catch ex As Exception
+                    e_firstname.Clear()
+                    TextBox1.Clear()
+                    MessageBox.Show("An error occurred: ")
+                End Try
+            End If
+
+        End If
 
 
-        Try
-            InsertQuery("admin_account", "username,password,employee_info", {e_firstname.Text, hashedPasswords, _employeeCode})
-            MessageBox.Show("Record inserted successfully.")
-            Me.Close()
 
-        Catch ex As Exception
-            e_firstname.Clear()
-            TextBox1.Clear()
-            MessageBox.Show("An error occurred: ")
-        End Try
 
 
     End Sub
