cd70e55092d278e852f82e6cec0b1a7077fd9dd0
alex tares <alexander.estares77@gmail.com>
2023-12-05 18:48:47 +0800

    change the design of button modify and change/add to function of update dep grid view and add modfiy form for delete and update department

---
+++ WinFormsApp2/modify_DepartmentForm.vb
@@ -0,0 +1,45 @@
+﻿Public Class modify_DepartmentForm
+
+
+    Private _depname As String
+    Private _depId As String
+
+
+    ' Modify the constructor to accept employeeCode as a parameter
+    Public Sub New(depname As String, depID As String)
+        InitializeComponent()
+
+        _depname = depname
+        _depId = depID
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub modify_DepartmentForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DeleteDepartment(_depname)
+    End Sub
+    Function deleteDepartment(depname As String)
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to delete this Department?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.Yes Then
+            ' User confirmed, proceed with deletion
+            UpdateQuery("employee_info", "department_id", {1}, "department_id=" & _depId)
+            DeleteQuery("qcu_department", "department_id=@id", {_depId})
+
+
+            admindashboardform.UpdateDeptGridView2()
+            MessageBox.Show("Department Data deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Me.Close()
+            Me.Close()
+        Else
+            ' User canceled deletion
+            MessageBox.Show("Deletion canceled.", "Canceled", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Function
+End Class
