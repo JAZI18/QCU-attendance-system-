e6150d2979875e0b2318b9bd759d06dedee12f02
Enerjhun relon <enerjhunrelon@gmail.com>
2023-12-14 00:10:39 +0800

    states and states manager added

---
+++ WinFormsApp2/main_form_states/states/UnlockingState.vb
@@ -0,0 +1,10 @@
+﻿Public Class UnlockingState
+    Inherits FinderState
+
+    Public Overrides Sub Enter()
+        form.Start_timer(Sub()
+                             ChangeState("FindingState")
+                         End Sub, "unlocking face", 600)
+    End Sub
+
+End Class
---
+++ WinFormsApp2/main_form_states/states/UnrecognizedFaceFoundState.vb
@@ -0,0 +1,21 @@
+﻿Public Class UnrecognizedFaceFoundState
+    Inherits FinderState
+    Public Overrides Sub Enter()
+
+
+        If Manager.prev_state.before.key = "UnrecognizedFaceFoundState" Then Exit Sub
+
+        form.pic_border.BackColor = Color.Red
+        Curr_emp_id = face_id
+        form.Refresh_fields()
+    End Sub
+
+    Public Overrides Sub Run()
+        If (FindFace(face_id)) Then
+            ChangeState("FoundState")
+            Exit Sub
+        End If
+        ChangeState("FindingState")
+    End Sub
+
+End Class
