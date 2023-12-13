564c5f842b12353d02b84f3aa927e4f634ade080
Enerjhun relon <enerjhunrelon@gmail.com>
2023-12-14 00:10:39 +0800

    states and states manager added

---
+++ WinFormsApp2/main_form_states/states/FindingState.vb
@@ -0,0 +1,11 @@
+﻿Public Class FindingState
+    Inherits FinderState
+
+
+    Public Overrides Sub enter()
+        form.pic_border.BackColor = Color.Blue
+        form.Refresh_fields()
+        Curr_emp_id = face_id
+    End Sub
+
+End Class
---
+++ WinFormsApp2/main_form_states/states/base_states/State.vb
@@ -0,0 +1,46 @@
+﻿Public MustInherit Class State
+
+    Public ReadOnly key As String
+    Private _manager1 As StateManager
+    Protected form As mainform
+    Public face_id As Integer = -10
+    Public before As State
+
+    Public Property Curr_emp_id As Object
+        Get
+            Return form.curr_emp_id
+        End Get
+        Set(value As Object)
+            form.curr_emp_id = value
+        End Set
+    End Property
+
+
+    Public Sub New()
+        key = Me.GetType.Name
+    End Sub
+
+    Public Property Manager As StateManager
+        Get
+            Return _manager1
+        End Get
+        Set(value As StateManager)
+            _manager1 = value
+            form = _manager1.parent
+        End Set
+    End Property
+
+    Public Sub ChangeState(key As String)
+        Manager.SetState(key)
+    End Sub
+
+    Public Overridable Sub Enter()
+    End Sub
+
+    Public Overridable Sub Run()
+    End Sub
+
+
+
+
+End Class
