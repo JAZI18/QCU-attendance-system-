2603db0ab9c037aed160a8171cc571ccd6867c76
Jay Pirnce <mangmangjayprince@gmail.com>
2023-12-14 14:02:55 +0800

    add employee

--- WinFormsApp2/addEmployee.Designer.vb
+++ WinFormsApp2/addEmployee.Designer.vb
@@ -22,8 +22,6 @@ Partial Class addEmployee
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        TabControl3 = New TabControl()
-        TabPage9 = New TabPage()
         submit_employees_btn = New Button()
         e_dep = New ComboBox()
         dep = New Label()
@@ -39,124 +37,95 @@ Partial Class addEmployee
         m = New Label()
         f = New Label()
         e_date = New DateTimePicker()
+        Panel1 = New Panel()
+        Label1 = New Label()
         PictureBox1 = New PictureBox()
-        TabControl3.SuspendLayout()
-        TabPage9.SuspendLayout()
+        Panel1.SuspendLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
-        ' TabControl3
-        ' 
-        TabControl3.Controls.Add(TabPage9)
-        TabControl3.Location = New Point(12, 147)
-        TabControl3.Name = "TabControl3"
-        TabControl3.SelectedIndex = 0
-        TabControl3.Size = New Size(494, 345)
-        TabControl3.TabIndex = 1
-        ' 
-        ' TabPage9
-        ' 
-        TabPage9.BackColor = Color.Transparent
-        TabPage9.Controls.Add(submit_employees_btn)
-        TabPage9.Controls.Add(e_dep)
-        TabPage9.Controls.Add(dep)
-        TabPage9.Controls.Add(e)
-        TabPage9.Controls.Add(e_email)
-        TabPage9.Controls.Add(e_lastname)
-        TabPage9.Controls.Add(e_middlename)
-        TabPage9.Controls.Add(e_firstname)
-        TabPage9.Controls.Add(d)
-        TabPage9.Controls.Add(e_gender)
-        TabPage9.Controls.Add(Label17)
-        TabPage9.Controls.Add(l)
-        TabPage9.Controls.Add(m)
-        TabPage9.Controls.Add(f)
-        TabPage9.Controls.Add(e_date)
-        TabPage9.Location = New Point(4, 24)
-        TabPage9.Name = "TabPage9"
-        TabPage9.Padding = New Padding(3)
-        TabPage9.Size = New Size(486, 317)
-        TabPage9.TabIndex = 0
-        TabPage9.Text = "TabPage9"
-        ' 
         ' submit_employees_btn
         ' 
-        submit_employees_btn.Location = New Point(211, 279)
+        submit_employees_btn.BackColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        submit_employees_btn.FlatStyle = FlatStyle.Flat
+        submit_employees_btn.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        submit_employees_btn.ForeColor = SystemColors.ButtonHighlight
+        submit_employees_btn.Location = New Point(545, 448)
         submit_employees_btn.Name = "submit_employees_btn"
-        submit_employees_btn.Size = New Size(193, 33)
-        submit_employees_btn.TabIndex = 15
+        submit_employees_btn.Size = New Size(118, 43)
+        submit_employees_btn.TabIndex = 30
         submit_employees_btn.Text = "Submit"
-        submit_employees_btn.UseVisualStyleBackColor = True
+        submit_employees_btn.UseVisualStyleBackColor = False
         ' 
         ' e_dep
         ' 
         e_dep.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
         e_dep.FormattingEnabled = True
-        e_dep.Location = New Point(6, 283)
+        e_dep.Location = New Point(394, 380)
         e_dep.Name = "e_dep"
         e_dep.Size = New Size(194, 29)
-        e_dep.TabIndex = 14
+        e_dep.TabIndex = 29
         ' 
         ' dep
         ' 
         dep.AutoSize = True
-        dep.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        dep.Location = New Point(6, 262)
+        dep.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        dep.Location = New Point(394, 359)
         dep.Name = "dep"
-        dep.Size = New Size(98, 21)
-        dep.TabIndex = 13
+        dep.Size = New Size(93, 21)
+        dep.TabIndex = 28
         dep.Text = "Department"
         ' 
         ' e
         ' 
         e.AutoSize = True
-        e.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        e.Location = New Point(211, 113)
+        e.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        e.Location = New Point(615, 166)
         e.Name = "e"
         e.Size = New Size(48, 21)
-        e.TabIndex = 11
+        e.TabIndex = 27
         e.Text = "Email"
         ' 
         ' e_email
         ' 
         e_email.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        e_email.Location = New Point(208, 134)
+        e_email.Location = New Point(612, 187)
         e_email.Name = "e_email"
         e_email.Size = New Size(206, 29)
-        e_email.TabIndex = 10
+        e_email.TabIndex = 26
         ' 
         ' e_lastname
         ' 
         e_lastname.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        e_lastname.Location = New Point(3, 134)
+        e_lastname.Location = New Point(391, 187)
         e_lastname.Name = "e_lastname"
         e_lastname.Size = New Size(194, 29)
-        e_lastname.TabIndex = 2
+        e_lastname.TabIndex = 18
         ' 
         ' e_middlename
         ' 
         e_middlename.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        e_middlename.Location = New Point(208, 65)
+        e_middlename.Location = New Point(612, 93)
         e_middlename.Name = "e_middlename"
         e_middlename.Size = New Size(206, 29)
-        e_middlename.TabIndex = 1
+        e_middlename.TabIndex = 17
         ' 
         ' e_firstname
         ' 
         e_firstname.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        e_firstname.Location = New Point(3, 65)
+        e_firstname.Location = New Point(391, 93)
         e_firstname.Name = "e_firstname"
         e_firstname.Size = New Size(194, 29)
-        e_firstname.TabIndex = 0
+        e_firstname.TabIndex = 16
         ' 
         ' d
         ' 
         d.AutoSize = True
-        d.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        d.Location = New Point(9, 186)
+        d.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        d.Location = New Point(397, 264)
         d.Name = "d"
-        d.Size = New Size(41, 21)
-        d.TabIndex = 9
+        d.Size = New Size(39, 21)
+        d.TabIndex = 25
         d.Text = "Dob"
         ' 
         ' e_gender
@@ -164,89 +133,125 @@ Partial Class addEmployee
         e_gender.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
         e_gender.FormattingEnabled = True
         e_gender.Items.AddRange(New Object() {"Male", "Female", "Binary", "Non-Binary", "Optimus Prime", "World Ender", "Others"})
-        e_gender.Location = New Point(208, 207)
+        e_gender.Location = New Point(612, 285)
         e_gender.Name = "e_gender"
         e_gender.Size = New Size(206, 29)
-        e_gender.TabIndex = 8
+        e_gender.TabIndex = 24
         ' 
         ' Label17
         ' 
         Label17.AutoSize = True
-        Label17.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label17.Location = New Point(211, 186)
+        Label17.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label17.Location = New Point(615, 264)
         Label17.Name = "Label17"
-        Label17.Size = New Size(64, 21)
-        Label17.TabIndex = 7
+        Label17.Size = New Size(61, 21)
+        Label17.TabIndex = 23
         Label17.Text = "Gender"
         ' 
         ' l
         ' 
         l.AutoSize = True
-        l.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        l.Location = New Point(6, 113)
+        l.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        l.Location = New Point(394, 166)
         l.Name = "l"
-        l.Size = New Size(79, 21)
-        l.TabIndex = 6
-        l.Text = "Lastname"
+        l.Size = New Size(81, 21)
+        l.TabIndex = 22
+        l.Text = "Last name"
         ' 
         ' m
         ' 
         m.AutoSize = True
-        m.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        m.Location = New Point(211, 44)
+        m.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        m.Location = New Point(615, 72)
         m.Name = "m"
-        m.Size = New Size(102, 21)
-        m.TabIndex = 5
-        m.Text = "Middlename"
+        m.Size = New Size(101, 21)
+        m.TabIndex = 21
+        m.Text = "Middle name"
         ' 
         ' f
         ' 
         f.AutoSize = True
-        f.Font = New Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        f.Location = New Point(6, 44)
+        f.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        f.Location = New Point(394, 72)
         f.Name = "f"
-        f.Size = New Size(81, 21)
-        f.TabIndex = 3
-        f.Text = "Firstname"
+        f.Size = New Size(83, 21)
+        f.TabIndex = 19
+        f.Text = "First name"
         ' 
         ' e_date
         ' 
         e_date.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        e_date.Location = New Point(6, 207)
+        e_date.Location = New Point(394, 285)
         e_date.Name = "e_date"
         e_date.Size = New Size(191, 29)
-        e_date.TabIndex = 4
+        e_date.TabIndex = 20
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.FromArgb(CByte(134), CByte(134), CByte(134))
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Location = New Point(-1, -1)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(351, 519)
+        Panel1.TabIndex = 31
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.ForeColor = Color.Lime
+        Label1.Location = New Point(140, 290)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(67, 25)
+        Label1.TabIndex = 33
+        Label1.Text = "Label1"
         ' 
         ' PictureBox1
         ' 
-        PictureBox1.Location = New Point(145, 23)
+        PictureBox1.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        PictureBox1.Location = New Point(11, 24)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(171, 166)
-        PictureBox1.TabIndex = 2
+        PictureBox1.Size = New Size(328, 239)
+        PictureBox1.TabIndex = 32
         PictureBox1.TabStop = False
         ' 
         ' addEmployee
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(468, 504)
-        Controls.Add(PictureBox1)
-        Controls.Add(TabControl3)
+        BackColor = Color.FromArgb(CByte(223), CByte(223), CByte(223))
+        ClientSize = New Size(868, 512)
+        Controls.Add(Panel1)
+        Controls.Add(submit_employees_btn)
+        Controls.Add(e_dep)
+        Controls.Add(dep)
+        Controls.Add(e)
+        Controls.Add(e_email)
+        Controls.Add(e_lastname)
+        Controls.Add(e_middlename)
+        Controls.Add(e_firstname)
+        Controls.Add(d)
+        Controls.Add(e_gender)
+        Controls.Add(Label17)
+        Controls.Add(l)
+        Controls.Add(m)
+        Controls.Add(f)
+        Controls.Add(e_date)
         FormBorderStyle = FormBorderStyle.FixedToolWindow
         Margin = New Padding(2)
         Name = "addEmployee"
         StartPosition = FormStartPosition.CenterParent
         Text = "addEmployee"
-        TabControl3.ResumeLayout(False)
-        TabPage9.ResumeLayout(False)
-        TabPage9.PerformLayout()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
-    Friend WithEvents TabControl3 As TabControl
-    Friend WithEvents TabPage9 As TabPage
     Friend WithEvents submit_employees_btn As Button
+    Friend WithEvents e_dep As ComboBox
     Friend WithEvents dep As Label
     Friend WithEvents e As Label
     Friend WithEvents e_email As TextBox
@@ -260,6 +265,7 @@ Partial Class addEmployee
     Friend WithEvents m As Label
     Friend WithEvents f As Label
     Friend WithEvents e_date As DateTimePicker
-    Friend WithEvents e_dep As ComboBox
+    Friend WithEvents Panel1 As Panel
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
 End Class
--- WinFormsApp2/addEmployee.vb
+++ WinFormsApp2/addEmployee.vb
@@ -11,25 +11,25 @@ Public Class addEmployee
         e_dep.SelectedIndex = 0
         reader.Close()
     End Sub
-    Private Sub submit_employees_btn_(sender As Object, e As EventArgs) Handles submit_employees_btn.Click
+    Private Sub submit_employees_btn_(sender As Object, e As EventArgs)
 
 
         MsgBox(e_dep.SelectedItem)
         Try
             InsertQuery("employee_info", "employee_code,first_name,middle_name,last_name,dob,gender,department_id,email",
-        {GenerateEmployeeCode(), e_firstname.Text, e_middlename.Text, e_lastname.Text, e_date.Value.ToString("yyyy/MM/dd"),
-         e_gender.SelectedItem.ToString, selectDepartment(), e_email.Text})
+        {GenerateEmployeeCode, e_firstname.Text, e_middlename.Text, e_lastname.Text, e_date.Value.ToString("yyyy/MM/dd"),
+         e_gender.SelectedItem.ToString, selectDepartment, e_email.Text})
             MessageBox.Show("Record inserted successfully.")
 
-            admindashboardform.updateEmpployeeGrid()
+            admindashboardform.updateEmpployeeGrid
             admindashboardform.Enabled = True
-            Me.Close()
+            Close
 
         Catch ex As Exception
-            e_firstname.Clear()
-            e_middlename.Clear()
-            e_lastname.Clear()
-            e_email.Clear()
+            e_firstname.Clear
+            e_middlename.Clear
+            e_lastname.Clear
+            e_email.Clear
             e_gender.SelectedItem = Nothing
             e_date.Value = Date.Now
             MessageBox.Show("An error occurred: ")
@@ -66,16 +66,11 @@ Public Class addEmployee
         Return employeeCode
     End Function
 
+    Private Sub submit_employees_btn_Click(sender As Object, e As EventArgs) Handles submit_employees_btn.Click
+        If e_firstname.Text Or e_lastname.Text Or e_middlename.Text Or e_date.Text Or e_email.Text Or e_gender.Text Or e_dep.Text = "" Then Exit Sub
+    End Sub
+
     Private Sub addEmployee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ListDepartment()
-    End Sub
-
-
-    Private Sub TabPage9_Click(sender As Object, e As EventArgs) Handles TabPage9.Click
-
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
     End Sub
 End Class
