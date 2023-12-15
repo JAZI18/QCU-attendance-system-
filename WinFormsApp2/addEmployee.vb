6d3ca476afe8fdfcaff31417b8e44e9f53e9b496
alex tares <alexander.estares77@gmail.com>
2023-12-15 12:22:53 +0800

    showDeparment and fix submit button

--- WinFormsApp2/addEmployee.vb
+++ WinFormsApp2/addEmployee.vb
@@ -11,32 +11,7 @@ Public Class addEmployee
         e_dep.SelectedIndex = 0
         reader.Close()
     End Sub
-    Private Sub submit_employees_btn_(sender As Object, e As EventArgs)
 
-
-        MsgBox(e_dep.SelectedItem)
-        Try
-            InsertQuery("employee_info", "employee_code,first_name,middle_name,last_name,dob,gender,department_id,email",
-        {GenerateEmployeeCode, e_firstname.Text, e_middlename.Text, e_lastname.Text, e_date.Value.ToString("yyyy/MM/dd"),
-         e_gender.SelectedItem.ToString, selectDepartment, e_email.Text})
-            MessageBox.Show("Record inserted successfully.")
-
-            admindashboardform.updateEmpployeeGrid
-            admindashboardform.Enabled = True
-            Close
-
-        Catch ex As Exception
-            e_firstname.Clear
-            e_middlename.Clear
-            e_lastname.Clear
-            e_email.Clear
-            e_gender.SelectedItem = Nothing
-            e_date.Value = Date.Now
-            MessageBox.Show("An error occurred: ")
-        End Try
-
-
-    End Sub
     Function selectDepartment() As Integer
         Dim reader As MySqlDataReader = SelectQuery("*", "qcu_department", {e_dep.SelectedItem}, "department_name=@deparment")
 
@@ -65,10 +40,34 @@ Public Class addEmployee
     End Function
 
     Private Sub submit_employees_btn_Click(sender As Object, e As EventArgs) Handles submit_employees_btn.Click
-        If e_firstname.Text = "" Or e_lastname.Text = "" Or e_middlename.Text = "" Or e_date.Text = "" Or e_email.Text = "" Or e_gender.Text = "" Or e_dep.Text = "" Then Exit Sub
+        If e_firstname.Text = "" Or e_lastname.Text = "" Or e_middlename.Text = "" Or e_date.Text = "" Or e_email.Text = "" Or e_gender.Text = "" Or e_dep.Text = "" Then
+
+            MsgBox(e_dep.SelectedItem)
+            Try
+                InsertQuery("employee_info", "employee_code,first_name,middle_name,last_name,dob,gender,department_id,email",
+        {GenerateEmployeeCode(), e_firstname.Text, e_middlename.Text, e_lastname.Text, e_date.Value.ToString("yyyy/MM/dd"),
+         e_gender.SelectedItem.ToString, selectDepartment(), e_email.Text})
+                MessageBox.Show("Record inserted successfully.")
+
+                admindashboardform.updateEmpployeeGrid()
+                admindashboardform.Enabled = True
+                Close()
+
+            Catch ex As Exception
+                e_firstname.Clear()
+                e_middlename.Clear()
+                e_lastname.Clear()
+                e_email.Clear()
+                e_gender.SelectedItem = Nothing
+                e_date.Value = Date.Now
+                MessageBox.Show("An error occurred: ")
+            End Try
+            Exit Sub
+        End If
     End Sub
 
     Private Sub addEmployee_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        ListDepartment()
+        e_gender.SelectedIndex = 0
     End Sub
 End Class
