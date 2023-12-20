fb1b594181127115f1fd63a8afbf3681fef4a10d
Enerjhun relon <enerjhunrelon@gmail.com>
2023-12-20 15:03:31 +0800

    implemented insert attendance funct

--- WinFormsApp2/mainform.vb
+++ WinFormsApp2/mainform.vb
@@ -7,7 +7,7 @@ Public Class mainform
 
 
     Dim facerecog As FaceRecognition
-
+    Dim last_attendance_id = -1
 
     ' WinAPI procedure to release HBITMAP handles returned by FSDKCam.GrabFrame
     Declare Auto Function DeleteObject Lib "gdi32.dll" (hObject As IntPtr) As Boolean
@@ -16,12 +16,25 @@ Public Class mainform
 
         Dim tin = time_in_tb.Text
         Dim tout = time_out_tb.Text
+        Dim id = employee_id_tb.Text
+
 
         Try
+
+            If tout <> "" Then
+                'assume that the record has already the time in
+                'sojust update the timeout part
+                UpdateQuery("emp_attendance",
+                            "time_out", {tout, id},
+                             "employee_id = @id AND emp_attendance.date = CURDATE()")
+                Exit Try
+            End If
+
             InsertQuery("emp_attendance", "employee_id,workday,date,time_in,time_out,over_time_in,over_time_out", {
-              employee_id_tb.Text, Now.ToString("dddd"), Now, tin, tout,
+              id, Now.ToString("dddd"), Now, tin, tout,
               overtime_in_tb.Text, overtime_out_tb.Text
         })
+
             MessageBox.Show(Me, $"SUCCESS!!! TIME {If(tout = "", "IN", "OUT")} : {If(tout = "", tin, tout)}", $"TIME {If(tout = "", "IN", "OUT")}", MessageBoxButtons.OK)
         Catch ex As Exception
             MessageBox.Show(Me, $"OOPS!!! SOMETHING WENT WRONG : {ex.Message} {vbCrLf} enter code to try again", $"TIME {If(tout = "", "IN", "OUT")}", MessageBoxButtons.OK)
@@ -65,6 +78,21 @@ Public Class mainform
                                         New UnlockingState()
                                         }, Me, cam_pic_box)
 
+        facerecog.Init()
+
+        While True
+            Try
+                facerecog.Create_tracker()
+
+                Exit While
+            Catch ex As Exception
+                Dim res As Integer = MessageBox.Show($"{ex.Message}", "oops!", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error)
+                If res = DialogResult.Cancel Then
+                    staffadminbtns.Show()
+                    Me.Close()
+                End If
+            End Try
+        End While
 
 
 
@@ -114,15 +142,41 @@ Public Class mainform
 
     Sub Fetch_employee_details(id As String)
 
+        Dim time_out = selectScalarQuery("time_out", "emp_attendance", {id}, "employee_id  = @eid AND emp_attendance.date = CURDATE()")
+
+        Try
+            If time_out.ToString() <> "00:00:00" Then
+                cam_pic_box.Image = My.Resources.roblox
+
+                Dim res = MessageBox.Show($"You have already TIMED OUT{vbCrLf}Do you want to continue?", "time out warning", MessageBoxButtons.YesNo)
+
+                If res = DialogResult.No Then
+                    Refresh_fields()
+                    Exit Sub
+                End If
+
+            End If
+        Catch ex As Exception
+
+        End Try
+
+
         Dim reader As MySqlDataReader = SelectQuery("*", "employee_info", {id}, "employee_id = @id")
 
         If reader.Read() Then
+
+
+
+
+
             fullname_lb.Text = $"{reader("last_name")}, {reader("first_name")} {reader("middle_name").substring(0, 1)}."
             Dim time_in = selectScalarQuery("CASE WHEN time_in IS  NULL THEN  'null'  ELSE time_in  END AS timed_in", "emp_attendance", {id}, "employee_id  = @eid AND emp_attendance.date = CURDATE()")
-            Dim ntime_in = If(time_in = Nothing, Now, CDate(time_in))
+            Dim ntime_in = If(time_in = Nothing, Now, CDate(time_in)).ToString("hh:mm tt")
 
-            time_in_tb.Text = ntime_in.ToString("hh:mm tt")
-            time_out_tb.Text = If(time_in = Nothing, "", Now)
+
+
+            time_in_tb.Text = ntime_in
+            time_out_tb.Text = If(time_in = Nothing, "", Now.ToString("hh:mm tt"))
             Exit Sub
         End If
         EmpNotFound()
@@ -140,13 +194,15 @@ Public Class mainform
         employee_code_tb.Clear()
 
         time_in_tb.Clear()
+        time_out_tb.Clear()
         fullname_lb.Text = "--------,----- ------"
         sched_lb.Text = "--:-- -- to --:-- --"
+        facerecog.Refresh()
     End Sub
 
 
     Sub Fetch_employee_scheds(id As String)
-        Dim reader As MySqlDataReader = SelectQuery("*", "employee_schedule", {id}, "employee_id = @id")
+        Dim reader As MySqlDataReader = SelectQuery("*", "employee_schedule", {id}, "employee_id     = @id")
         If reader.Read() Then
             sched_lb.Text = $"{New DateTime(1, 1, 1).Add(reader("shift_start_time")).ToString("hh:mm tt")} to {New DateTime(1, 1, 1).Add(reader("shift_end_time")).ToString("hh:mm tt")}"
         End If
@@ -161,7 +217,8 @@ Public Class mainform
 
         facerecog.Save_tracker()
 
-            facerecog.Close_cam()
+        facerecog.Close_cam()
     End Sub
+
 
 End Class
